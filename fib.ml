631bcdb977d3d1c0c355f43016460937406fa463
DevMMing <mming@stuy.edu>
2019-02-27 12:45:08 -0500

    final push

--- fib.ml
+++ fib.ml
@@ -1,5 +1,8 @@
 let rec fib i =
-  if i <= 1 then 1 else fib (i - 1) + fib (i - 2)
-
-print_int (fib 46);
-print_newline ()
+  if i <= 1 then 1 else fib (i - 1) + fib (i - 2);;
+let rec fib n = match n with
+| 0 -> 0
+| 1 -> 1
+| i -> fib (i - 1) + fib (i - 2);;
+print_int (fib 38);;
+print_newline ();;
